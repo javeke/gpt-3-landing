c32f733e67e1c6d46d2e8c523eb9325a22246309
Javier <javier.bryan@speurgroup.com>
2021-12-14 00:09:47 -0500

    added vue config and deploy script

---
+++ deploy.sh
@@ -0,0 +1,21 @@
+#! /usr/bin/env sh
+
+# abort on errors
+
+set -e
+
+# build
+
+npm run build
+
+cd dist
+
+git init
+
+git add .
+
+git commit -m 'deploy'
+
+git push -f git@github.com:javeke/gpt-3-landing.git master:gh-pages
+
+cd ..
